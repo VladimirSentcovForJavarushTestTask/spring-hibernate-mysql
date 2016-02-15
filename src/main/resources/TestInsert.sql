f5ff5fa134e8fdb96b62cd1d813d96d2dd535c4e
VladimirSentcovForJavarushTestTask <vladimirsencov@gmail.ru>
2016-02-15 12:04:49 +0300

    New File VCS

--- src/main/resources/TestInsert.sql
+++ src/main/resources/TestInsert.sql
@@ -8,7 +8,7 @@ INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
 VALUES ('3', 'Ivan', '35', FALSE, '2016-02-15 11:26:59');
 
 INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
-VALUES ('3', 'Olga', '30', FALSE, '2016-02-15 11:27:58');
+VALUES ('4', 'Olga', '30', FALSE, '2016-02-15 11:27:58');
 
 INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
-VALUES ('3', 'Natalia', '30', FALSE, '2016-02-15 11:32:58');
+VALUES ('5', 'Natalia', '30', FALSE, '2016-02-15 11:32:58');
