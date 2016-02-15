ea538aa486985965e73171729966a0b52391d0ca
VladimirSentcovForJavarushTestTask <vladimirsencov@gmail.ru>
2016-02-15 12:03:38 +0300

    New File VCS

---
+++ src/main/resources/TestInsert.sql
@@ -0,0 +1,14 @@
+INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
+VALUES ('1', 'Vladimir', '29', TRUE, '2016-02-15 11:25:37');
+
+INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
+VALUES ('2', 'Petr', '29', FALSE, '2016-02-15 11:25:57');
+
+INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
+VALUES ('3', 'Ivan', '35', FALSE, '2016-02-15 11:26:59');
+
+INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
+VALUES ('3', 'Olga', '30', FALSE, '2016-02-15 11:27:58');
+
+INSERT INTO `test`.`user` (`ID`, `NAME`, `AGE`, `IS_ADMIN`, `CREATED_DATE`)
+VALUES ('3', 'Natalia', '30', FALSE, '2016-02-15 11:32:58');
